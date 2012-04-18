20d470e394c05a1164729177aad1c31b53bbaba5
Kirk Beitz <kirkbeitz@san.rr.com>
2012-04-18 03:25:36 -0700

    add capability to track a transient cell while retrieving info to populate it with that's not local

---
+++ TransientSubTitleTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  TransientSubTitleTableViewCell.h
+//  FlikrTop
+//
+//  Created by me on 2012.04.17.
+//  Copyright (c) 2012 xolaware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransientSubTitleTableViewCell : UITableViewCell
+
+@property (strong, nonatomic) NSString* transientId;
+
+@end
---
+++ TransientSubTitleTableViewCell.m
@@ -0,0 +1,15 @@
+//
+//  TransientSubTitleTableViewCell.m
+//  FlikrTop
+//
+//  Created by me on 2012.04.17.
+//  Copyright (c) 2012 xolaware. All rights reserved.
+//
+
+#import "TransientSubTitleTableViewCell.h"
+
+@implementation TransientSubTitleTableViewCell
+
+@synthesize transientId = _transientId;
+
+@end
