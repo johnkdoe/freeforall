97b712914d406df234073984f216a7f796e01e1e
Kirk Beitz <kirkbeitz@san.rr.com>
2012-06-29 02:51:16 -0700

    renamed xolawareBackground to better reflect its true nature

---
+++ xolawareBackground.m
@@ -0,0 +1,49 @@
+//
+//  xolawareBackground.m
+//  voyeur
+//
+//  Created by me on 2012.06.28.
+
+#include "xolawareOpenSourceCopyright.h"	//  Copyright (c) 2012 xolaware.
+
+#import "xolawareBackground.h"
+
+@interface xolawareBackground ()
+{
+	xolawareBackgroundTaskBlock backgroundTaskBlockMember;
+}
+
+@end
+
+@implementation xolawareBackground
+
++ (xolawareBackground*)retriever:(xolawareBackgroundTaskBlock)backgroundTaskBlock {
+	return [[xolawareBackground alloc] initWithTask:backgroundTaskBlock];
+}
+
+- (xolawareBackground*)initWithTask:(xolawareBackgroundTaskBlock)backgroundTaskBlock
+{
+	self = [self init];
+	if (self)
+	{
+		backgroundTaskBlockMember = backgroundTaskBlock;
+	}
+	return self;
+}
+
+- (id)getData {
+	UIBackgroundTaskIdentifier taskId
+	  = [[UIApplication sharedApplication] beginBackgroundTaskWithExpirationHandler:NULL];
+
+		[UIApplication sharedApplication].networkActivityIndicatorVisible = YES;
+
+		id result = backgroundTaskBlockMember();
+
+		[UIApplication sharedApplication].networkActivityIndicatorVisible = NO;
+	
+	[[UIApplication sharedApplication] endBackgroundTask:taskId];
+
+	return result;
+}
+
+@end
