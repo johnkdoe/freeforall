79a1430f574f679b0f9f8c4468edf94a0765a6c0
Kirk Beitz <kirkbeitz@san.rr.com>
2012-07-02 17:39:42 -0700

    add normalizeFontSizeUsingCellAccessoryType:
    0) saves the storyboardFontSize from the first visit for use in all subsequent calls
    1) takes an arg that considers the type of cell this was (helps when re-used, and the label frame size changed because of the accessory type
    2) calculate and adjustment based on the difference in width between an accessory button and detailed disclosure button (WARNING: magic number 13)
    3) use sizeWithFont:minFontSize:actualFontSize:forWidth:lineBreakMode: to calculate the fontSize desired
    4) use that as the font-size so when the table re-draws this cell with this label field, it does the right thing.

--- ImageTitleDescriptionDisclosureTableViewCell.m
+++ ImageTitleDescriptionDisclosureTableViewCell.m
@@ -17,11 +17,37 @@
 
 @synthesize photoId = _photoId;
 
+static CGFloat _storyboardFontSize;
+
 - (BOOL)isEqual:(id)object
 {
 	return [object isKindOfClass:[ImageTitleDescriptionDisclosureTableViewCell class]]
 		&& [[object photoId] isEqualToString:self.photoId];
 		 
+}
+
+- (void)normalizeFontSizeUsingCellAccessoryType:(UITableViewCellAccessoryType)reusedType
+{
+	UIFont* titleFont = self.itddTitle.font;
+	if (!_storyboardFontSize)
+		_storyboardFontSize = titleFont.pointSize;	// set once and re-use
+
+	CGFloat widthAdjustment = 0;;
+	if (reusedType != self.accessoryType)
+		switch (self.accessoryType)
+		{
+		  case UITableViewCellAccessoryDetailDisclosureButton:	widthAdjustment = 13; break;
+		  case UITableViewCellAccessoryDisclosureIndicator:		widthAdjustment = -13; break;
+		  default:;
+		}
+
+	CGFloat actualFontSize;
+	[self.itddTitle.text sizeWithFont:[titleFont fontWithSize:_storyboardFontSize]
+						  minFontSize:self.itddTitle.minimumFontSize
+					   actualFontSize:&actualFontSize
+							 forWidth:self.itddTitle.frame.size.width+widthAdjustment-2
+						lineBreakMode:UILineBreakModeWordWrap];
+	self.itddTitle.font = [titleFont fontWithSize:(CGFloat)((int)actualFontSize)];
 }
 
 @end
