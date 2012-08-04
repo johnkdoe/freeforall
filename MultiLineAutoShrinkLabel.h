e3712b9999586d2df8d970ae275d10805e0085b3
Kirk Beitz <kirkbeitz@san.rr.com>
2012-08-04 00:39:37 -0700

    an extension of UILabel that auto shrinks the text size for multiline labels

---
+++ MultiLineAutoShrinkLabel.h
@@ -0,0 +1,13 @@
+//
+//  MultiLineAutoShrinkLabel.h
+//	xolawareUI
+//
+//  Created by xolaware on 2012.08.02.
+
+#include "xolawareOpenSourceCopyright.h"	//  Copyright (c) 2012 xolaware.
+
+#import <UIKit/UIKit.h>
+
+@interface MultiLineAutoShrinkLabel : UILabel
+- (void)quoteAutoshrinkUnquote;
+@end
