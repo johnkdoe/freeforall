70b5ae7b8d2c97857813f87e90e55e4ae4975ce5
Kirk Beitz <kirkbeitz@san.rr.com>
2012-11-02 18:52:48 -0700

    a trick to allow upside-down orientation when building for both iOS 5 & iOS 6

---
+++ UpsideDownRotation_6_0.h
@@ -0,0 +1,20 @@
+//
+//  UpsideDownRotation_6_0.h
+//
+//	this code is for use in apps for which it's desirable to allow
+//	the upside-down interface orientation on both iOS 5 and iOS 6
+//
+//  Created by kb on 2012.10.30.
+
+#import "xolawareOpenSourceCopyright.h"
+
+#import <UIKit/UIKit.h>
+
+@interface UINavigationController(UpsideDownRotation_6_0)
+@end
+
+@interface UISplitViewController(UpsideDownRotation_6_0)
+@end
+
+@interface UITabBarController(UpsideDownRotation_6_0)
+@end
