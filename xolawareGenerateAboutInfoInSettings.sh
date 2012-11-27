f4fb67d01c717d833a459167cd608a4c46249563
johnkdoe <john.k.doe@gmail.com>
2012-11-27 10:52:14 -0800

    script to auto-generate version in about group as first thing in system settings

---
+++ xolawareGenerateAboutInfoInSettings.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# this works based on a combination of recipes found at:
+# http://stackoverflow.com/a/10348517/774691
+# http://mrox.net/blog/2008/11/16/adding-debug-only-preferences-in-iphone-applications/
+# 
+# it differs from the stackoverflow answer in the following ways:
+# - it assumes the -info.plist is per target
+# - it assumes the usual Xcode default of creating a product directory containing the -info.plist
+# - it assumes per-target naming of .plist files
+# - it uses ${PRODUCT_NAME}/${TARGET_NAME} env-vars from Xcode to achieve this
+# - it assumes there's a PSGroupSpecifier "About" containing the version as the first item
+# - it assumes the version is already a PSTitleValueSpecifier, and is always first in Root.plist
+# - it takes the "outputfile" as an argument, so that it can be invoked as shown in mrox's blog
+#
+# mrox's blog is helpful to show how to create the build phase that runs this script.
+# the script invocation needs to be different in the following ways:
+# - it is concerned with version information only, and thus not debug-only
+# - it is invoked using Input File $(SRCROOT)/${PRODUCT_NAME}/Resources/Settings.bundle/Root.plist
+# - it is invoked using File $(TARGET_BUILD_DIR)/$(FULL_PRODUCT_NAME)/Settings.bundle/Root.plist
+
+set -e
+
+PLISTBUDDYCMD="/usr/libexec/PlistBuddy -c"
+
+CFBundleShortVersionString=`${PLISTBUDDYCMD} "Print :CFBundleShortVersionString" ${SRCROOT}/${PRODUCT_NAME}/${TARGET_NAME}-Info.plist`
+CFBundleVersion=`${PLISTBUDDYCMD} "Print :CFBundleVersion" ${SRCROOT}/${PRODUCT_NAME}/${TARGET_NAME}-Info.plist`
+${PLISTBUDDYCMD} "Set :PreferenceSpecifiers:1:DefaultValue '${CFBundleShortVersionString} (b${CFBundleVersion})'" $1
