bb7db518b04ffc334935f92fe7e369a4c503f6df
johnkdoe <john.k.doe@gmail.com>
2012-12-04 03:52:42 -0800

    a pair of scripts to hold then later restore the Reources/Settings.bundle/Root.plist
    this allows system settings that will display
    (a) version info that changes regularly and (b) developer debug definitions

---
+++ xolawareStashRestoreSettingsBundleRootPlist.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#
+# should be run as the last script in Build Phases, after the Copy Bundle Resources Phase
+
+echo '-- Manual Restore $INFOPLIST_FILE Script --'
+
+ROOT_PLIST=${PROJECT}/Resources/Settings.bundle/Root.plist
+
+set -e
+
+# first, see if it was stashed earlier due to uncommitted changes
+if [ -e ${TARGET_TEMP_DIR}/Root.plist ]; then
+	echo mv ${TARGET_TEMP_DIR}/Root.plist ${ROOT_PLIST}
+	mv ${TARGET_TEMP_DIR}/Root.plist ${ROOT_PLIST}
+
+# the better option when available: restore to the pristine state
+elif [ `git status --porcelain ${ROOT_PLIST}|wc -l` -gt 0 ]; then
+	echo git checkout -- ${ROOT_PLIST}
+	git checkout -- ${ROOT_PLIST}
+fi
+
---
+++ xolawareStashSettingsBundleRootPlist.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#
+# should be run prior to the Copy Bundle Resources step
+# and prior to any version information modifier scripts
+
+echo '-- Temp Hold Settings.bundle/Root.plist Script --'
+
+ROOT_PLIST=${PROJECT}/Resources/Settings.bundle/Root.plist
+
+set -e
+
+# a fallback in case the user has made changes to the file
+if [ `git status --porcelain ${ROOT_PLIST}]|wc -l` -gt 0 ]; then
+	echo cp -p ${ROOT_PLIST} ${TARGET_TEMP_DIR}
+	cp -p ${ROOT_PLIST} ${TARGET_TEMP_DIR}
+fi
