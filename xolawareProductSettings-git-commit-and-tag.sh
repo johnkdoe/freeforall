6f72339a332dd5b1bb5baa8a0356414d606350f1
johnkdoe <john.k.doe@gmail.com>
2012-12-04 20:57:24 -0800

    why bother re-creating Info.plist location; ${INFOPLIST_FILE} has it

--- xolawareProductSettings-git-commit-and-tag.sh
+++ xolawareProductSettings-git-commit-and-tag.sh
@@ -5,15 +5,11 @@
 
 echo "-- ${INFOPLIST_FILE} git commit & tag Install Script --"
 
-SCRIPT_INFO_PLIST=${PROJECT_DIR}/${PROJECT}/$INFOPLIST_FILE
-
-set -e
-
-SCRIPT_VERSION=`/usr/libexec/PlistBuddy -c 'Print :CFBundleShortVersionString' ${SCRIPT_INFO_PLIST}`
-SCRIPT_BUILD_NUMBER=`/usr/libexec/Plistbuddy -c 'Print :CFBundleVersion' ${SCRIPT_INFO_PLIST}`
+SCRIPT_VERSION=`/usr/libexec/PlistBuddy -c 'Print :CFBundleShortVersionString' ${INFOPLIST_FILE}`
+SCRIPT_BUILD_NUMBER=`/usr/libexec/Plistbuddy -c 'Print :CFBundleVersion' ${INFOPLIST_FILE}`
 if [ `git status --porcelain ${SCRIPT_INFO_PLIST}|wc -l` -gt 0 ]; then
-	echo git commit -m \"version ${SCRIPT_VERSION} build ${SCRIPT_BUILD_NUMBER} \" ${SCRIPT_INFO_PLIST}
-	git commit -m \"version ${SCRIPT_VERSION} build ${SCRIPT_BUILD_NUMBER} \" ${SCRIPT_INFO_PLIST}
+	echo git commit -m '"'version ${SCRIPT_VERSION} build ${SCRIPT_BUILD_NUMBER}'"' ${INFOPLIST_FILE}
+	git commit -m "version ${SCRIPT_VERSION} build ${SCRIPT_BUILD_NUMBER}" ${INFOPLIST_FILE}
 fi
 echo git tag -f ${SCRIPT_VERSION}
 git tag -f -F /dev/null ${SCRIPT_VERSION}
