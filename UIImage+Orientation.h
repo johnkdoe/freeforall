d3030cfba5df24de60ea223b6530366532edff9d
johnkdoe <john.k.doe@gmail.com>
2013-01-02 16:18:12 -0800

    return a new UIimage re-oriented to upright based on internal info

---
+++ UIImage+Orientation.h
@@ -0,0 +1,13 @@
+//
+//  UIImage+Orientation.h
+//  athlete
+//
+//  Created by kb on 2013.01.02.
+//  Copyright (c) 2013 kb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Orientation)
+- (UIImage*)reorientedImage;
+@end
